16ec6da751d5cbd13b9ce2ec0490918a5bed5b58
manojpawarsj12 <manojpawarsj.mp11@gmail.com>
2020-12-30 12:18:25 +0530

    initial typescript express brr

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("lel nub coder ")
+import express from "express";
+let app :any = express();
+
+app.get("/", (req, res) => {
+  res.send("typescript nub");
+});
+
+
+app.listen(3000, () => {
+  console.log("http://localhost:3000/");
+});
