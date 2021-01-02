6edac22f61a03de27fa8f38c08cf63b44a06e52c
manojpawarsj12 <manojpawarsj.mp11@gmail.com>
2021-01-02 23:15:32 +0530

    huehue jwt auth maybe

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,2 +1,44 @@
 import * as jwt from "jsonwebtoken";
+import { getRepository } from "typeorm";
+import { User } from "../entities/User";
 
+const user_repo = getRepository(User);
+const maxAge: number = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.jwt_key, {
+    expiresIn: maxAge,
+  });
+};
+export function login_get(_req, res) {
+  res.json("login get req sir");
+}
+export function signup_get(_req, res) {
+  res.json("signup get req sir");
+}
+
+export async function signup_post(req, res) {
+  try {
+    const { username, email, FirstName, LastName, age, password } = req.body;
+    const user = user_repo.create({
+      username,
+      email,
+      FirstName,
+      LastName,
+      age,
+      password,
+    });
+    await user.save().then(() => {
+      res.json("record saved");
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function login_post(req,res){
+    try{
+        const { username, password } = req.body;
+        
+
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import express from "express";
 import { createConnection } from "typeorm";
 import cors from "cors";
 import helmet from "helmet";
+import dotenv from "dotenv";
 import compression from "compression";
 import auth_router from "./routes/authroutes"
 const app = express();
-
+dotenv.config();
 app.use(cors());
 app.use(helmet());
 app.use(compression());
