91214f74265bbf580991f5ae36da596ee57e1048
manojpawarsj12 <manojpawarsj.mp11@gmail.com>
2021-01-02 14:02:25 +0530

    idk?

--- src/routes/authroutes.ts
+++ src/routes/authroutes.ts
@@ -1,8 +1,12 @@
-import { Router } from "express";
-
-const auth_router = Router();
-
-auth_router.get("/", (_req, res) => {
+import { auth_router } from "express";
+// import authController from "./controllers/authController"
+const auth_auth_router = auth_router();
+// auth_router.get('/signup', authController.signup_get);
+// auth_router.post('/signup', authController.signup_post);
+// auth_router.get('/login', authController.login_get);
+// auth_router.post('/login', authController.login_post);
+// auth_router.get('/logout', authController.logout_get);
+auth_auth_router.get("/", (_req, res) => {
   res.send("typescript nub");
 });
-export default auth_router;
+export default auth_auth_router;
